a29c163f61fe21527b8151d5dae90d4d5e95c732
Yasu <raityou615@gmail.com>
2023-08-10 19:04:21 +0000

    課題提出

--- 1-12.rb
+++ 1-12.rb
@@ -16,13 +16,13 @@ puts add(100, 200)
 
 #3
 def times(arr)
-  x = 0
-  x arr.shift()
+  x = 1
+  arr.shift()
   arr.each do |num|
     x *= num
   end
   return x
-  end
+end
 
 puts times([1, 3, 5, 7, 9])
 
@@ -30,18 +30,12 @@ puts times([1, 3, 5, 7, 9])
 def max_array(arr)
   #とりあえず配列の最初の要素を一番大きい値とする
   max_number = arr[0]
-  
-  arr each do |a|
-    #↓課題4↓
-    if max_number > 0{
-      return a
-    else
-      not return
-    }
-    
-  end
-  
+  arr.each do |a|
+    if max_number < a
+       max_number = a
+    end
     return max_number
-  end
+    end
+end
   
-  puts max_array(arr[1, 2, 3, 4, 5, 6, 7, 8, 9])
+  puts max_array([1, 2, 3, 4, 5, 6, 7, 8, 9])
